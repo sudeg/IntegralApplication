006cfbefed113117b7b8ec3fc960f8063685f79e
sudeg <sudegonul95@gmail.com>
2020-09-25 21:49:43 +0300

    login/sign-up created

---
+++ IntegralApplication/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  IntegralApplication
+//
+//  Created by sude gonul on 9/25/20.
+//  Copyright © 2020 sude gonul. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+    }
+    
+}
